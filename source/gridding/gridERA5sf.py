362effb0f1bd671b6de313b96ac72d4b2543dd74
ac <7801186+ac137@users.noreply.github.com>
2020-08-27 14:54:03 -0400

    fix to create grids with appropriate resolution when dx is changed in main function

--- source/gridding/gridERA5sf.py
+++ source/gridding/gridERA5sf.py
@@ -43,7 +43,7 @@ from config import figure_path
 def main(year, startMonth=0, endMonth=4, dx=50000, extraStr='v11', data_path=reanalysis_raw_path+'ERA5/', out_path=forcing_save_path+'Precip/ERA5/', fig_path=figure_path+'Precip/ERA5/', anc_data_path='../../anc_data/'):
 
 
-	xptsG, yptsG, latG, lonG, proj = cF.create_grid()
+	xptsG, yptsG, latG, lonG, proj = cF.create_grid(dxRes=dx)
 	print(xptsG)
 	print(yptsG)
 
