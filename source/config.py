47ae661e086e4d4f55e3f82406112b29648f112c
akpetty <alekpetty@gmail.com>
2020-12-18 14:01:39 -0500

    added dummy folders:

--- source/config.py
+++ source/config.py
@@ -1,20 +1,21 @@
 """ config.py
 	
 	Configuration file for the main NESOSIM function
-
-	To do: add model constants/parameter settings here
 	
 """
 
 
 #----- File paths (replace with links to the raw and gridded forcings) -------------
 
-nsidc_raw_path = ''
-osisaf_raw_path = ''
-cdr_raw_path = ''
-reanalysis_raw_path = ''
-forcing_save_path = ''
-model_save_path = ''
-figure_path = ''
-oib_data_path=''
+# Assuming running things from source dir
+
+nsidc_raw_path = '../test_forcings/IceDrift/NSIDCv4/'
+cdr_raw_path = '../test_forcings/IceConc/CDR/'
+reanalysis_raw_path = '../test_forcings/'
+forcing_save_path = '../forcings/'
+model_save_path = '../output/'
+figure_path = '../figures/'
 anc_data_path='../../anc_data/'
+
+osisaf_raw_path = ''
+oib_data_path=''
